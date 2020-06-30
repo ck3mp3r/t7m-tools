9fa7238255bf85ffceece231b0194813be02c614
Christian Kemper <christian.kemper@me.com>
2020-06-30 02:35:12 +0100

    quick logic update...

--- tf.sh
+++ tf.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-action="plan -detailed-exitcode"
 color="-no-color"
-validate=0
 
 while [[ "$#" -gt 0 ]]; do case $1 in
   -e|--env) env="$2"; shift;;
   -i|--init) init=1;;
   -c|--color) color="";;
+  plan) action="plan -detailed-exitcode";;
   *) action=$1;;
 esac; shift; done
 
